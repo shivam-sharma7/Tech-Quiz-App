e13748fea5b754968357888016027defd5cb3eb4
Shivam Sharma <meshivam81@gmail.com>
2023-04-09 07:56:09 +0530

    Quiz app initialize

---
+++ script.js
@@ -0,0 +1,75 @@
+const quizData = [
+    {
+
+        question: 'How old you are?',
+        a: '18',
+        b: '21',
+        c: '20',
+        d: '25',
+        correct: 'c'
+
+    },
+
+    {
+
+        question: 'which is best Programming language for beginner?',
+        a: 'C/C++',
+        b: 'Java',
+        c: 'Python',
+        d: 'Javascript',
+        correct: 'c'
+
+    },
+
+    {
+
+        question: 'what is HTML?',
+        a: 'A Programming language',
+        b: 'API',
+        c: 'Hypertext Markup Language',
+        d: 'JSON object',
+        correct: 'c'
+
+    },
+
+    {
+      
+        question: 'Which tech use to create a website?',
+        a: 'Web Development',
+        b: 'DevOps',
+        c: 'Machine Learning',
+        d: 'AndroidnDevelopment',
+        correct: 'a'
+
+    },
+
+    {
+
+        question: 'What is best programming language in 2023?',
+        a: 'C/C++',
+        b: 'Java',
+        c: 'Python',
+        d: 'Javascript',
+        correct: 'd'
+    },
+
+    {
+
+        question: 'which is best fron-end framework in 2023?',
+        b: 'Angular',
+        a: 'React',
+        c: 'Vue',
+        d: `jQuery`,
+        correct: 'b'
+    },
+
+     {
+
+        question: 'Who is founder of Twitter?',
+        a: 'Elon Musk',
+        b: 'Mark Zuckerburge',
+        c: 'jack Dursey',
+        d: 'Larry Page',
+        correct: 'c'
+     }
+]
