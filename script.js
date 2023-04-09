eed04f6858063339b58a4b1b299c73a63978a85f
Shivam Sharma <meshivam81@gmail.com>
2023-04-09 12:02:59 +0530

    tech quiz app update

--- script.js
+++ script.js
@@ -73,19 +73,34 @@ const quizData = [
      }
 ];
 
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer');
 const questionE1 = document.getElementById('question');
 
- const a_text = document.getElementById('a_text');
- const b_text = document.getElementById('b_text');
- const c_text = document.getElementById('c_text');
- const d_text = document.getElementById('d_text');
- const submitBtn = document.getElementById('submit');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score = 0;
 
+const deselectAns = () =>{
+
+    answerEls.forEach((answerEl) =>{
+      answerEl.checked = false;
+    });
+ 
+}
+
+ 
 const loadQuiz = () => {
-    const currentQuizData = quizData[currentQuiz];
 
+    deselectAns();
+    
+    const currentQuizData = quizData[currentQuiz];
+    
     questionE1.innerText = currentQuizData.question;
 
     a_text.innerText = currentQuizData.a;
@@ -97,9 +112,39 @@ const loadQuiz = () => {
 
 loadQuiz();
 
+ 
+const getSelect = () =>{
+       
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) =>{
+         
+        if(answerEl.checked){
+            answer =  answerEl.id;
+        }
+    });
+
+    return answer;
+};
+
+ 
 submitBtn.addEventListener('click', () =>{
+// check to see ans
+const answer = getSelect();
+
+if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+        score++;
+    }
+
     currentQuiz++;
-
-    loadQuiz();
-
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        quiz.innerHTML = `
+            <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+            
+            <button onclick="location.reload()">Reload</button>`;
+    }
+}
 });
